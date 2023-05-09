4affc789e29c472883e73572077befcd461bc7c0
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-05-09 09:02:19 +0000

    index.js

---
+++ data/index.js
@@ -0,0 +1,21 @@
+
+
+let name = "christ";
+let greeting = `hello ${name}`;
+console.log (greeting)
+
+let message = "you must obey what i told you";
+let kimba = `sulaiman ${message}`;
+console.log (kimba);
+
+let one = "hello,";
+let two = "how are you?";
+let output1 = (`${one} ${two}`);
+console.log (output1);
+
+let song = "fight the youth";
+let score = 9;
+let highscore = 10;
+let output = `i like the song ${song}. i give it a score of ${(score/highscore)*100}`;
+console.log (output);
+
